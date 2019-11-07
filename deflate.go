705866092bba1b4fb166ac7c7bc0cc576171dca4
yuukimiyo <yuuki.miyo@gmail.com>
2019-11-07 09:06:53 +0900

    add fileutility

--- deflate.go
+++ deflate.go
@@ -1,46 +1,44 @@
 package totext
 
 import (
-    "io"
-    "bytes"
-    "compress/zlib"
+	"bytes"
+	"compress/zlib"
 	"encoding/base64"
+	"io"
 )
 
-
+// Deflate is function for deflate text.
 func Deflate(text string) (string, error) {
+	zlibBuffer, err := compressStringToZlib(text)
+	if err != nil {
+		panic(err)
+	}
 
-    zlibBuffer, err := compressStringToZlib(text)
-    if err != nil {
-        panic(err)
-    }
-
-	base64String, _ := convertBytesToBase64String(zlibBuffer.Bytes())
-    if err != nil {
-        panic(err)
-    }
+	base64String := convertBytesToBase64String(zlibBuffer.Bytes())
 
 	return base64String, nil
 }
 
 func compressStringToZlib(text string) (*bytes.Buffer, error) {
-
 	textBuffer := bytes.NewBufferString(text)
-    zlibBuffer := new(bytes.Buffer)
-    zlibWriter := zlib.NewWriter(zlibBuffer)
-    defer zlibWriter.Close()
+	zlibBuffer := new(bytes.Buffer)
+	zlibWriter := zlib.NewWriter(zlibBuffer)
 
-    if _, err := io.Copy(zlibWriter, textBuffer); err != nil {
-        return nil, err
-    }
-    return zlibBuffer, nil
+	defer zlibWriter.Close()
+
+	if _, err := io.Copy(zlibWriter, textBuffer); err != nil {
+		return nil, err
+	}
+
+	return zlibBuffer, nil
 }
 
-func convertBytesToBase64String(b []byte) (string, error) {
+func convertBytesToBase64String(b []byte) string {
 	base64String := base64.StdEncoding.EncodeToString(b)
-	return base64String, nil
+	return base64String
 }
 
+// Inflate is function for inflate base64text.
 func Inflate(base64String string) (string, error) {
 
 	bytes, err := convertBase64StringToBytes(base64String)
@@ -57,25 +55,27 @@ func Inflate(base64String string) (string, error) {
 }
 
 func convertBase64StringToBytes(base64String string) ([]byte, error) {
-
 	bytes, err := base64.StdEncoding.DecodeString(base64String)
-    if err != nil {
+	if err != nil {
 		return nil, err
-    }
+	}
 
 	return bytes, nil
 }
 
 func decompressZlibToString(zlibBytes []byte) (string, error) {
-
 	bytesReader := bytes.NewReader(zlibBytes)
+
 	zlibReader, err := zlib.NewReader(bytesReader)
-    if err != nil {
+	if err != nil {
 		return "", err
-    }
+	}
 
 	zlibBuffer := new(bytes.Buffer)
-	zlibBuffer.ReadFrom(zlibReader)
+	err := zlibBuffer.ReadFrom(zlibReader)
+	if err != nil {
+		return "", err
+	}
 
 	return zlibBuffer.String(), nil
 }
