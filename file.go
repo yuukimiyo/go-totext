f32fb29eb13c32a7cf1a1135f1209be6df95c73e
yuukimiyo <yuuki.miyo@gmail.com>
2019-11-20 10:45:37 +0900

    add WriteLines()

--- file.go
+++ file.go
@@ -4,10 +4,37 @@ import (
 	"bufio"
 	"io"
 	"io/ioutil"
+	"os"
 	"path/filepath"
 )
 
-// ReadLineOld is old function for iterate lines on bufio.Reader.
+// ReadLine is new function to iterate lines on bufio.Reader.
+// lineBuffer is byte slice, you can use as make([]byte, 0, 1024*1024) or more simply []byte.
+// example:
+// var rd = bufio.NewReaderSize(fp, bufferSize)
+// for {
+//     line, err := readLine(rd, make([]byte, 0, 1024*1024))
+//     if err != nil {
+//         if err == io.EOF {
+//             break
+//         }
+//         panic(err)
+//     }
+//     lines = append(lines, line)
+// }
+func ReadLine(reader *bufio.Reader, lineBuffer []byte) (string, error) {
+	for {
+		l, hasNext, e := reader.ReadLine()
+		lineBuffer = append(lineBuffer, l...)
+
+		if !hasNext || e != nil {
+			return string(lineBuffer), e
+		}
+	}
+}
+
+// ReadLineOld is old function to iterate lines on bufio.Reader.
+// *** this is old function. ***
 // The example value for limitBytes is 1000000.
 func ReadLineOld(rd *bufio.Reader, limitBytes int) (string, bool, error) {
 	iseof := false
@@ -34,29 +61,37 @@ func ReadLineOld(rd *bufio.Reader, limitBytes int) (string, bool, error) {
 	return string(buf), iseof, nil
 }
 
-// ReadLine is new function for iterate lines on bufio.Reader.
-// lineBuffer is byte slice, you can use as make([]byte, 0, 1024*1024) or more simply []byte.
-// example:
-// var rd = bufio.NewReaderSize(fp, bufferSize)
-// for {
-//     line, err := readLine(rd, make([]byte, 0, 1024*1024))
-//     if err != nil {
-//         if err == io.EOF {
-//             break
-//         }
-//         panic(err)
-//     }
-//     lines = append(lines, line)
-// }
-func ReadLine(reader *bufio.Reader, lineBuffer []byte) (string, error) {
-	for {
-		l, hasNext, e := reader.ReadLine()
-		lineBuffer = append(lineBuffer, l...)
+// WriteLines is function to write string array.
+func WriteLines(filename string, lines []string, linesep string, modeflag string, permission os.FileMode) error {
+	mode := os.O_WRONLY | os.O_CREATE
+	if modeflag == "a" {
+		mode = os.O_WRONLY | os.O_APPEND
+	}
 
-		if !hasNext || e != nil {
-			return string(lineBuffer), e
+	f, err := os.OpenFile(filename, mode, permission)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	for _, line := range lines {
+		_, err := f.WriteString(line + linesep)
+		if err != nil {
+			return err
 		}
 	}
+
+	return nil
+}
+
+//MakeDir is function to make dir.
+// Make dir if not exists.
+func MakeDir(path string) error {
+	err := os.Mkdir(path, 0755)
+	if err == nil || os.IsExist(err) {
+		return nil
+	}
+	return err
 }
 
 // Dirs is function to get dir list from dirpath.
